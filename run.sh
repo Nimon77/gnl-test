0298bbc146dc92f600343b17ba7998b24ab4e144
Nicolas SIMON <nsimon@student.42.fr>
2020-01-16 15:15:21 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ cd ../42TESTERS
 sh all_tests.sh
 cd ../Get_Next_Line_Tester
 bash run.sh
-cd ../GNL_lover
-cp ../get_next_line* copy_in_here_GNL_files/.
-sh GNL_lover.sh
 cd ../gnl_tester
 cp ../get_next_line* .
 bash run_tests.sh
 cd ../gnlkiller
 cp ../get_next_line* .
 sh test.sh
+cd ../GNL_lover
+cp ../get_next_line* copy_in_here_GNL_files/.
+sh GNL_lover.sh
 
