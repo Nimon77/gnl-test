b524a1ab6ae336be415a883c9ffc47c845c206a2
Nicolas SIMON <nsimon@student.42.fr>
2020-01-16 15:22:12 +0100

    add gnlkiller/test* modif run.sh

--- run.sh
+++ run.sh
@@ -14,19 +14,23 @@ bash run.sh
 
 read -n 1 -s
 
+cd ../GNL_lover
+cp ../get_next_line* copy_in_here_GNL_files/.
+sh GNL_lover.sh
+
+read -n 1 -s
+
 cd ../gnl_tester
 cp ../get_next_line* .
-bash run_tests.sh
+bash run_tests.sh 5
+bash run_tests.sh 10
+bash run_tests.sh 20
+bash run_tests.sh 42
+bash run_tests.sh 10000000
 
 read -n 1 -s
 
 cd ../gnlkiller
 cp ../get_next_line* .
 sh test.sh
-
-read -n 1 -s
-
-cd ../GNL_lover
-cp ../get_next_line* copy_in_here_GNL_files/.
-sh GNL_lover.sh
 
