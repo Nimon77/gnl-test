b91a6fbfe45b7d828c55d276d7ac1dec1e161f12
Nicolas SIMON <nsimon@student.42.fr>
2020-01-16 15:18:03 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,31 @@
 cp ../get_next_line* .
 cd 42cursus_gnl_tests
 make
-cd ../42TESTERS
+
+read -n 1 -s
+
+cd ../42TESTERS-GNL
 sh all_tests.sh
+
+read -n 1 -s
+
 cd ../Get_Next_Line_Tester
 bash run.sh
+
+read -n 1 -s
+
 cd ../gnl_tester
 cp ../get_next_line* .
 bash run_tests.sh
+
+read -n 1 -s
+
 cd ../gnlkiller
 cp ../get_next_line* .
 sh test.sh
+
+read -n 1 -s
+
 cd ../GNL_lover
 cp ../get_next_line* copy_in_here_GNL_files/.
 sh GNL_lover.sh
