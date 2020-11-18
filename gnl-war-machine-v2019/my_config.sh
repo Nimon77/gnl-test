666b9a38ef89328fb602d53cdc24bae71d43fb65
nsimon <nsimon@student.42.fr>
2020-11-18 20:52:18 +0100

    update config and rights management

--- gnl-war-machine-v2019/my_config.sh
+++ gnl-war-machine-v2019/my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_GNL="../../get_next_line"
+PATH_GNL="./"
 
 #If you use a relative path, don't put double quotes.
 #For example :
